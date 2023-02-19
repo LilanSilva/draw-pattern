c434114e4e7089ac1b63e528dbbd0fcc778483a9
Lilan Silva <lilan@rozie.ai>
2023-02-19 21:03:13 +0100

    Merge all script files into one

--- README.md
+++
@@ -1,2 +0,0 @@
-# draw-pattern
-Draw pattern
--- script.js
+++ script.js
@@ -6,7 +6,109 @@ let patternQuantity = 10;
 let outerRatio = 0.7;
 let imageType = "svg";
 
+/**************************** Transform ******************************/
+// Constants
+const SHAPE_TYPES = [
+  '<svg height="@@size" width="@@size" transform="@@rotation" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 300 300" style="fill: @@color;" xml:space="preserve"><path d="M150,0C67.29,0,0,67.29,0,150v150h112.5h75H300V150C300,67.29,232.71,0,150,0z M147.95,222.92\n c-39.14,0-70.87-31.73-70.87-70.87s31.73-70.87,70.87-70.87c39.14,0,70.87,31.73,70.87,70.87S187.09,222.92,147.95,222.92z"/></svg>', 
+  '<svg height="@@size" width="@@size" transform="@@rotation" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 300 300" style="fill: @@color;" xml:space="preserve"><path d="M300,300H187.5c0-103.39-84.11-187.5-187.5-187.5V0C165.42,0,300,134.58,300,300z"/></svg>', 
+  '<svg height="@@size" width="@@size" transform="@@rotation" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 300 300" style="fill: @@color;" xml:space="preserve"><path d="M300,300H150C67.29,300,0,232.71,0,150S67.29,0,150,0h150v112.5H150c-20.68,0-37.5,16.82-37.5,37.5\n s16.82,37.5,37.5,37.5h150V300z"/></svg>'];
+const SHAPE_ROTATIONS = [0, 90, 180, 270];
 
+// Get random number
+function getRandomInt(max) {
+  return Math.floor(Math.random() * Math.floor(max));
+}
+
+// Get rotate status
+function getRandomRotation() {
+  let rotation = getRandomInt(4);
+
+  switch (rotation) {
+    case 1:
+      return 'rotate(90)';
+    case 2:
+      return 'rotate(180)';
+    case 3:
+      return 'rotate(270)';
+    default:
+      return '';
+  }
+}
+
+// Transform shape according to provide settings
+function transformShape(colorOptions, size) {
+  var shape = SHAPE_TYPES[getRandomInt(SHAPE_TYPES.length)];
+
+  shape = shape.replaceAll('@@size', (size + 'px'));
+  shape = shape.replace('@@rotation', getRandomRotation());
+  shape = shape.replace('@@color', colorOptions[getRandomInt(colorOptions.length)]);
+
+  return shape;
+}
+
+/************************* Utility Support ****************************/
+// Export the pattern as SVG
+function exportSvg() {
+  var canvas = document.getElementById("pattern-svg");
+  var context = canvas.getContext("2d");
+  var imageData = context.getImageData(0, 0, canvas.width, canvas.height);
+
+  // Create an SVG image element with the same dimensions as the canvas
+  var svg = '<svg xmlns="http://www.w3.org/2000/svg" width="' + canvas.width + '" height="' + canvas.height + '">';
+
+  // Loop through the pixels of the canvas and add them to the SVG image
+  for (var i = 0; i < imageData.data.length; i += 4) {
+    var r = imageData.data[i];
+    var g = imageData.data[i + 1];
+    var b = imageData.data[i + 2];
+    var a = imageData.data[i + 3];
+
+    svg += '<rect x="' + ((i / 4) % canvas.width) + '" y="' + Math.floor(i / (4 * canvas.width)) + '" width="1" height="1" fill="rgba(' + r + ',' + g + ',' + b + ',' + (a / 255) + ')" />';
+  }
+  svg += '</svg>';
+
+  var blob = new Blob([svg], { type: 'image/svg+xml' });
+  var url = URL.createObjectURL(blob);
+
+  var link = document.createElement('a');
+  link.setAttribute('href', url);
+  link.setAttribute('download', "pattern.svg");
+  link.style.display = 'none';
+
+  document.body.appendChild(link);
+  link.click();
+
+  document.body.removeChild(link);
+  URL.revokeObjectURL(url);
+}
+
+// Export the pattern as PNG
+function exportPng() {
+  const canvas = document.getElementById('pattern-svg');
+  const dataURL = canvas.toDataURL("image/png");
+
+  const link = document.createElement("a");
+  link.download = "pattern.png";
+  link.href = dataURL;
+
+  // Simulate a click on the link element to trigger the download
+  document.body.appendChild(link);
+  link.click();
+  document.body.removeChild(link);
+}
+
+// Remove specific item from array
+function removeItem(array, item) {
+  const index = array.indexOf(item);
+  if (index > -1) {
+    array.splice(index, 1);
+  }
+
+  return array;
+}
+
+
+/************************** Draw image ***********************/
 // Resize screen 
 function resizeScreen() {
   const rightMenu = document.getElementById("right-menu");
@@ -126,7 +228,8 @@ function generateCanvas(gridInfo) {
   }
 }
 
-// Event binder
+/************************** Event binder ***********************/
+// Bind page event
 function eventBinder() {
   // Add a click event listener to each label
   const formatLabels = document.querySelectorAll('label[for^="format-"]');
